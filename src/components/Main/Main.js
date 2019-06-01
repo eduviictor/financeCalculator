c3d73351d3a8b4fe3747c65146c0b26911c12690
eduviictor <eduvictornobrega@gmail.com>
2019-06-01 01:06:00 -0300

    Ajustando tela inicial

--- src/components/Main/Main.js
+++ src/components/Main/Main.js
@@ -1,29 +1,15 @@
 import React, { Component } from 'react';
-import { FlatList, Text, View, Button, StyleSheet, TouchableOpacity } from 'react-native';
+import { Text, View, StyleSheet, TouchableOpacity } from 'react-native';
 
-// const initialState = {
-//   0: {
-//     title: 'Juros Simples'
-//   },
-//   1: {
-//     title: 'Juros Compostos'
-//   }
-// }
 
 export default class Main extends Component {
-
-  // componentDidMount(){
-  //   this.setState({
-  //     itensMenu: initialState
-  //   })
-  // }
 
   static navigationOptions = {
     title: 'Calculadora Financeira',
     headerStyle: {
-      backgroundColor: '#1a237e',
+      backgroundColor: '#ccc',
     },
-    headerTintColor: '#fff',
+    headerTintColor: 'black',
     headerTitleStyle: {
       fontWeight: 'bold',
     },
@@ -38,36 +24,46 @@ export default class Main extends Component {
   render() {
     return (
       <View style={styles.container}>
-        <TouchableOpacity 
-          onPress={() => {this.props.navigation.push('JurosSimples')}}
-          style={styles.button}
-        >
-          <Text style={styles.textButton}>Juros Simples</Text>
-        </TouchableOpacity>
-        <TouchableOpacity 
-          onPress={() => {this.props.navigation.push('JurosCompostos')}}
-          style={styles.button}
-        >
-          <Text style={styles.textButton}>Juros Compostos</Text>
-        </TouchableOpacity>
-        <TouchableOpacity
-          onPress={() => {this.props.navigation.push('DescontoSimples')}}
-          style={styles.button}
-        >
-          <Text style={styles.textButton}>Desconto Simples</Text>
-        </TouchableOpacity>
-        <TouchableOpacity
-          onPress={() => {this.props.navigation.push('DescontoComposto')}}
-          style={styles.button}
-        >
-          <Text style={styles.textButton}>Desconto Composto</Text>
-        </TouchableOpacity>
+        <View style={styles.row}>
+          <TouchableOpacity 
+            onPress={() => {this.props.navigation.push('JurosSimples')}}
+            style={Object.assign({
+              backgroundColor: '#EEE2DF'
+            }, styles.button)}
+          >
+            <Text style={styles.textButton}>Juros Simples</Text>
+          </TouchableOpacity>
+
+          <TouchableOpacity 
+            onPress={() => {this.props.navigation.push('JurosCompostos')}}
+            style={Object.assign({
+              backgroundColor: '#C89F9C'
+            }, styles.button)}
+          >
+            <Text style={styles.textButton}>Juros Compostos</Text>
+          </TouchableOpacity>
+        </View>
+        
+        <View style={styles.row}>
+          <TouchableOpacity
+            onPress={() => {this.props.navigation.push('DescontoSimples')}}
+            style={Object.assign({
+              backgroundColor: '#C89F9C'
+            }, styles.button)}
+          >
+            <Text style={styles.textButton}>Desconto Simples</Text>
+          </TouchableOpacity>
+
+          <TouchableOpacity
+            onPress={() => {this.props.navigation.push('DescontoComposto')}}
+            style={Object.assign({
+              backgroundColor: '#EEE2DF'
+            }, styles.button)}
+          >
+            <Text style={styles.textButton}>Desconto Composto</Text>
+          </TouchableOpacity>
+        </View>
       </View>
-      // <FlatList 
-      // data={this.state.itensMenu}
-      // renderItem={() => this.renderMenu()}
-      
-      // />
     )
   }
 }
@@ -76,22 +72,25 @@ export default class Main extends Component {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
+    justifyContent: 'flex-start',
+    // alignItems: 'stretch',
     backgroundColor: '#ccc',
   },
+  row: {
+    flex: 1,
+    flexDirection: 'row'
+  },
   button: {
-    backgroundColor: '#534bae',
-    padding: 10,
-    borderRadius: 5,
-    borderColor: 'black',
-    borderWidth: 1,
-    width: 150,
-    marginTop: 10
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    // borderColor: 'black',
+    // borderWidth: 1,
+    width: '50%',
   },
   textButton: {
     fontWeight: 'bold',
-    color: 'white',
-    fontSize: 16
+    color: 'black',
+    fontSize: 18
   }
 });
